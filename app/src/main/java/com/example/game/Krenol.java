54d6b164ec1c6a198c8b7442ae9a10e74845a7b2
Ilya <novastacer@gmail.com>
2024-02-05 00:12:19 +0400

    Added buttons added a reaction to the buttons added the lines: CROSS and ZERO

--- app/src/main/java/com/example/game/Krenol.java
+++ app/src/main/java/com/example/game/Krenol.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class Krenol extends AppCompatActivity {
 
     Button button1,button2,button3,button4,button5,button6,button7,button8,button9;
-
+    private static final String CROSS = "X",ZERO = "0";
 
     TextView result;
     @Override
@@ -29,53 +29,52 @@ public class Krenol extends AppCompatActivity {
         button9 = findViewById(R.id.button9);
         result = findViewById(R.id.result);
 
-        cross = "X";
-        zero = "0";
+
     }
 
     public void clickButton1(View view) {
-        button1.setText(cross);
+        button1.setText(CROSS);
         isWinner();
     }
 
     public void clickButton2(View view) {
-        button2.setText(cross);
+        button2.setText(CROSS);
         isWinner();
     }
 
     public void clickButton3(View view) {
-        button3.setText(cross);
+        button3.setText(CROSS);
         isWinner();
     }
 
     public void clickButton4(View view) {
-        button4.setText(cross);
+        button4.setText(CROSS);
         isWinner();
     }
 
     public void clickButton5(View view) {
-        button5.setText(cross);
+        button5.setText(CROSS);
         isWinner();
     }
     public void clickButton6(View view) {
-        button6.setText(cross);
+        button6.setText(CROSS);
         isWinner();
     }
     public void clickButton7(View view) {
-        button7.setText(cross);
+        button7.setText(CROSS);
         isWinner();
     }
     public void clickButton8(View view) {
-        button8.setText(cross);
+        button8.setText(CROSS);
         isWinner();
     }
     public void clickButton9(View view) {
-        button9.setText(cross);
+        button9.setText(CROSS);
         isWinner();
     }
 
     public void isWinner(){
-        if(button1.getText() == cross && button2.getText() == cross && button3.getText() == cross){
+        if(button1.getText() == CROSS && button2.getText() == CROSS && button3.getText() == CROSS){
             result.setText("Поздравляем победа");
         }
     }
